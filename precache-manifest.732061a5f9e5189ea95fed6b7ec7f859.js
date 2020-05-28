60b2924f9fddb55ef9d175561e47b58ae7db74ef
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-05-28 13:39:08 +0300

    add build to the repo

---
+++ precache-manifest.732061a5f9e5189ea95fed6b7ec7f859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77c6c798e12cf0ed8a6ab1f17957eb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0599ff2a8d9a202f5698",
+    "url": "/static/css/main.1b602158.chunk.css"
+  },
+  {
+    "revision": "45add839b6b0c770eb0a",
+    "url": "/static/js/2.9d5031a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9d5031a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0599ff2a8d9a202f5698",
+    "url": "/static/js/main.eb8854f0.chunk.js"
+  },
+  {
+    "revision": "e58ad769b2453461ad73",
+    "url": "/static/js/runtime-main.9512471c.js"
+  }
+]);
