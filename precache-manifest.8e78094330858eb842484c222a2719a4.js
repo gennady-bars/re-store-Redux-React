17f27eb3e483b7fa0ae84e6e53e627b858dc8280
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-06-24 22:32:10 +0300

    Updates

---
+++ precache-manifest.8e78094330858eb842484c222a2719a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9136be9a928a2fa6468e201364158376",
+    "url": "./index.html"
+  },
+  {
+    "revision": "62814f965bb73bef9301",
+    "url": "./static/css/main.858867ae.chunk.css"
+  },
+  {
+    "revision": "b87e9c3dcd3bcc9922c2",
+    "url": "./static/js/2.eb0c89c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.eb0c89c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62814f965bb73bef9301",
+    "url": "./static/js/main.04191993.chunk.js"
+  },
+  {
+    "revision": "6a4a039a4bda7951d50b",
+    "url": "./static/js/runtime-main.81baab57.js"
+  }
+]);
